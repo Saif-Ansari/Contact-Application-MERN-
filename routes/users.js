4ef16c1b100f19416c0c902de7d51fe08e35fbe6
saifcr7 <sfansari007@gmail.com>
2020-09-05 04:27:51 +0530

    completed db setup and user model validation

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
+const { check, validationResult } = require("express-validator/check");
 
 // @route  POST  api/users
 // @desc   Register a user
 // @access Public
-router.post('/', (req,res)=>{
-    res.send(' Registered a user')
-});
+router.post(
+  "/",
+  [
+    check("name", "Please enter name").not().isEmpty(),
+    check("email", "please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password of minimum 6 characters"
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    return res.send("Passed");
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-const express = require('express');
-
+const express = require("express");
+const connectDB = require("./config/db");
 const app = express();
 
-app.get('/', (req,res) => res.json({msg:' Hello from server'}))
+//connect database
+connectDB();
+
+//Init Middleware
+
+app.use(express.json({ extended: false }));
+app.get("/", (req, res) => res.json({ msg: " Hello from server" }));
 
 //Define Routes
-app.use('/api/users', require('./routes/users'))
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/contacts', require('./routes/contacts'))
-const PORT = process.env.PORT ||  5000;
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=> console.log(`server is up at port ${PORT}`))
+app.listen(PORT, () => console.log(`server is up at port ${PORT}`));
